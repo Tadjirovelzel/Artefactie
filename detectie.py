49a2f114fa38e0c6b8bab16c94a2d90c1c570e53
Mrtz-Gl <moritzf.galisch@gmail.com>
2026-03-31 13:35:35 +0200

    detectie bestand

---
+++ detectie.py
@@ -0,0 +1,4 @@
+'''
+Deelt het databestand op in stukken en kijkt door middel van een fast fourier transform
+of een opvallende verandering plaats vindt in het signaal. Dat kan duiden op een artefact.
+'''
